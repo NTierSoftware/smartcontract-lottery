fa08738b8f93264f188d6f8423db5ee7f29a67b2
NTierSoftware <16448136+NTierSoftware@users.noreply.github.com>
2021-12-21 10:40:16 -0800

    fund_with_link()
    minor improvement of if logic.  
    removed needless assignment if the account and/or link_token already assigned.

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -81,8 +81,8 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
 def fund_with_link(
     contract_address, account=None, link_token=None, amount=100000000000000000
 ):  # 0.1 LINK
-    account = account if account else get_account()
-    link_token = link_token if link_token else get_contract("link_token")
+    if not account: account = get_account()
+    if not link_token: link_token = get_contract("link_token")
     tx = link_token.transfer(contract_address, amount, {"from": account})
     # link_token_contract = interface.LinkTokenInterface(link_token.address)
     # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
